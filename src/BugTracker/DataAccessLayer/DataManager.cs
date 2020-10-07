8965007a825bafd9368c50395bbad0d5e826a1af
Ruben A. Romero <romeroa.ruben@gmail.com>
2020-10-07 20:50:24 -0300

    DataManager

--- src/BugTracker/DataAccessLayer/DataManager.cs
+++ src/BugTracker/DataAccessLayer/DataManager.cs
@@ -81,6 +81,34 @@ public class DataManager : IDisposable
         }
     }
 
+    public DataTable ConsultaSQLStoreProcedure(string spName, Dictionary<string, object> prs = null)
+    {
+        SqlCommand cmd = new SqlCommand();
+        DataTable tabla = new DataTable();
+        try
+        {
+            cmd.Connection = dbConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = spName;
+
+            //Agregamos a la colección de parámetros del comando los filtros recibidos
+            if (prs != null)
+            {
+                foreach (var item in prs)
+                {
+                    cmd.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+
+            tabla.Load(cmd.ExecuteReader());
+            return tabla;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+    }
+
     /// Resumen:
     ///     Se utiliza para sentencias SQL del tipo “Insert/Update/Delete”. Recibe por valor una sentencia sql como string
     /// Devuelve:
@@ -108,6 +136,44 @@ public class DataManager : IDisposable
             cmd.CommandType = CommandType.Text;
             // Establece la instrucción a ejecutar
             cmd.CommandText = strSql;
+
+            //Agregamos a la colección de parámetros del comando los filtros recibidos
+            if (prs != null)
+            {
+                foreach (var item in prs)
+                {
+                    cmd.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+
+            // Retorna el resultado de ejecutar el comando
+            rtdo = cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return rtdo;
+    }
+
+    public int EjecutarSQLStoreProcedure(string spName, Dictionary<string, object> prs = null)
+    {
+        // Se utiliza para sentencias SQL del tipo “Insert/Update/Delete”
+
+        SqlCommand cmd = new SqlCommand();
+
+        int rtdo = 0;
+
+        // Try Catch Finally
+        // Trata de ejecutar el código contenido dentro del bloque Try - Catch
+        // Si hay error lo capta a través de una excepción
+        // Si no hubo error
+        try
+        {
+            cmd.Connection = dbConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            // Establece la instrucción a ejecutar
+            cmd.CommandText = spName;
 
             //Agregamos a la colección de parámetros del comando los filtros recibidos
             if (prs != null)
